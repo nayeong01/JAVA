287f86ee073851b8977a3dba9d02d3ed71645e18
nayeong01 <108383043+nayeong01@users.noreply.github.com>
2022-07-10 20:59:07 +0900

    자바 챕터2 예제문제

---
+++ java2_12.java
@@ -0,0 +1,13 @@
+
+class java2_12 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d = 85.4;
+		int score = (int)d;
+		
+		System.out.println("score="+score);
+		System.out.println("d="+d);
+
+	}
+
+}

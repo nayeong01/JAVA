578ce33c1d89af3e764efdabdce7ffffb7f4c2a1
nayeong01 <108383043+nayeong01@users.noreply.github.com>
2022-07-14 19:08:10 +0900

    자바 CH6 예제문제

---
+++ java6_2.java
@@ -0,0 +1,32 @@
+
+class Tv {
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() {power=!power;}
+	void channelUp() {++channel;}
+	void channelDown() {--channel;}
+	
+}
+
+
+public class java6_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tv t1 = new Tv();
+		Tv t2 = new Tv();
+		
+		System.out.println("t1의 channel값은 "+ t1.channel +" 입니다.");
+		System.out.println("t2의 channel값은 "+ t2.channel +" 입니다.");
+		
+		t1.channel = 7;
+		System.out.println("t1의 channel을 7로 변경하였습니다.");
+		
+		System.out.println("t1의 channel값은 "+t1.channel+" 입니다.");
+		System.out.println("t2의 channel값은 "+t2.channel+" 입니다.");
+
+	}
+
+}
